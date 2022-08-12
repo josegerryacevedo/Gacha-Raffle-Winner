4062cb504c08b75cfe2fb611da500371ebb96adc
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-08-12 10:08:24 +0800

    [Feature 7] Invite Page "Adjusment using render layout"

--- app/controllers/users/invite_people_controller.rb
+++ app/controllers/users/invite_people_controller.rb
@@ -4,18 +4,22 @@ class Users::InvitePeopleController < ApplicationController
   before_action :generate_qrcode
 
   def url
+    if current_user
     @url="#{request.base_url}/users/sign_up?promoter=#{current_user&.email}"
+    else
+      @url="#{request.base_url}/users/sign_up"
+    end
   end
 
   def generate_qrcode
     qrcode = RQRCode::QRCode.new(@url)
     @svg = qrcode.as_svg(
-      color: "000",
+      color: "FFF",
       shape_rendering: "crispEdges",
       module_size: 11,
       standalone: true,
       use_path: true,
-      fill: "00FFFF"
+      fill: "ffffff00"
     )
   end
 end
