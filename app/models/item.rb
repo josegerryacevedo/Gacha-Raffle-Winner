ee9f07c54678c2ab673d8758c0cf245e5944d0cf
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-08-15 10:45:31 +0800

    [Feature 11] Item Soft delete

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,10 @@ class Item < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   enum status: [:Inactive, :Active]
+
+  default_scope { where(deleted_at: nil) }
+
+  def destroy
+    update(deleted_at: Time.current)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_12_064746) do
+ActiveRecord::Schema.define(version: 2022_08_15_023829) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "genre"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2022_08_12_064746) do
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at"
   end
 
   create_table "provinces", force: :cascade do |t|
