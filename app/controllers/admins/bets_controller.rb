4bb01c0db76afe3f95b0a2984927e8d12a27d585
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-08-31 10:05:25 +0800

    [Feature 15] Bet Model "Add cancel action"

--- app/controllers/admins/bets_controller.rb
+++ app/controllers/admins/bets_controller.rb
@@ -1,4 +1,6 @@
 class Admins::BetsController < AdminController
+  before_action :set_bet, only: :cancel
+
   def index
     @bets = Bet.includes(:user, :item)
     @bets = @bets.where(serial_number: params[:serial_number]) if params[:serial_number].present?
@@ -6,5 +8,20 @@ class Admins::BetsController < AdminController
     @bets = @bets.where(user: {email: params[:email]}) if params[:email].present?
     @bets = @bets.where(state: params[:state]) if params[:state].present?
     @bets = @bets.where(created_at: params[:start].to_datetime..params[:end].to_datetime) if params[:start].present? && params[:end].present?
+  end
+
+  def cancel
+    if @bet.cancel!
+      flash[:notice] = "Successfully Cancelled!"
+    else
+      flash[:alert] = @bet.errors.full_messages.join(', ')
+    end
+    redirect_to admins_bets_path
+  end
+
+  private
+
+  def set_bet
+    @bet = Bet.find(params[:bet_id])
   end
 end
