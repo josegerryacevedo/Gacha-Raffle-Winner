415c4c2986703da5e689feddbdcb9858653c0be2
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-09-06 17:30:40 +0800

    [Feature 23] Client Profile Page

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -1,5 +1,10 @@
 class Users::ProfilesController < ApplicationController
   before_action :authenticate_user!
 
-  def show ;end
+  def show
+    @orders = Order.where(user: current_user) if params[:history] == 'order' || params[:history].blank?
+    @bets = Bet.includes(:item).where(user: current_user) if params[:history] == 'bet'
+    @invites = User.where(parent: current_user) if params[:history] == 'invite'
+    @winners = Winner.includes(:item, :bet).where(user: current_user).won if params[:history] == 'winner'
+  end
 end
