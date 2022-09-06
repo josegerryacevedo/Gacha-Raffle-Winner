360c3b9519358c02b72555059d4bd24c8f8d33ea
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-09-06 15:13:12 +0800

    Adjust codes

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :offer, optional: true
   enum genre: [:deposit, :increase, :deduct, :bonus, :share]
-  after_create :serial_number
+  after_create :generate_serial_number
 
   include AASM
   aasm column: :state do
@@ -14,13 +14,13 @@ class Order < ApplicationRecord
       transitions from: :pending, to: :submitted
     end
 
+    event :pay do
+      transitions from: :submitted, to: :paid, guard: :check_user_coin, after: [:pay_change_user_coins, :increase_total_deposit, :check_user_coin]
+    end
+
     event :cancel do
       transitions from: [:pending, :submitted], to: :cancelled
       transitions from: :paid, to: :cancelled, after: [:cancel_change_user_coins, :deduct_total_deposit]
-    end
-
-    event :pay do
-      transitions from: :submitted, to: :paid, guard: :check_user_coin, after: [:pay_change_user_coins, :increase_total_deposit, :check_user_coin]
     end
   end
 
@@ -49,7 +49,7 @@ class Order < ApplicationRecord
   end
 
   def check_user_coin
-    if (user.coin >= coin) && !deduct?
+    if (user.coins >= coin) && !deduct?
       true
     else
       error.base(base: 'Not Enough Coin!')
@@ -57,7 +57,7 @@ class Order < ApplicationRecord
     end
   end
 
-  def serial_number
+  def generate_serial_number
     ActiveRecord::Base.connection.execute("UPDATE `orders` SET `orders`.`serial_number` = CONCAT(DATE_FORMAT(CONVERT_TZ(orders.created_at, '+00:00', '+8:00'), '%y%m%d'),'-',#{id},'-',#{user.id},'-',
                                                   (SELECT LPAD(count(*), 4, 0)
                                                    FROM `orders` where `orders`.`user_id` = #{user.id}))
